34d8997d944515729cba5fbff6914ef3aec8ac5a
Sean Murdock <scmurdock@gmail.com>
2021-09-07 09:16:38 -0600

    Add bluetooth low energy service

--- wallmount_stedi_bluetooth.ino
+++ wallmount_stedi_bluetooth.ino
@@ -8,6 +8,10 @@
  * 
  * 
  */
+#include <ArduinoBLE.h>
+
+BLEService wallMountService("stedibalancev300bluetoothlenerg");
+BLEUnsignedCharCharacteristic distanceSensorCharacteristic("1212", BLERead | BLENotify);
 
 int triggerPin=2;
 int echoPin=3;
@@ -15,15 +19,55 @@ int echoPin=3;
 long duration;
 int distance;
 
-void setup() {
+void setup(){
   pinMode(triggerPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  Serial.begin(9600);//Serial Communication startgs at 9600 rate
-  Serial.println("Sensor Test");
+  pinMode(LED_BUILTIN, OUTPUT);
   
+  Serial.begin(9600);
+  while(!Serial);
+
+  if(!BLE.begin())
+  {
+    Serial.println("Starting BLE failed!");
+    while(1);
+  }
+
+  BLE.setLocalName("STEDIWallMountV3.00");
+  BLE.setAdvertisedService(wallMountService);
+  wallMountService.addCharacteristic(distanceSensorCharacteristic);
+  BLE.addService(wallMountService);
+
+  BLE.advertise();
+  Serial.println("Bluetooth STEDI device active, waiting for connections ...");  
 }
 
-void loop() {
+void loop(){
+
+  getDistance();
+
+  BLEDevice central = BLE.central();
+
+  if (central)
+  {
+    Serial.println("Connected to central: ");
+    Serial.println(central.address());
+    digitalWrite(LED_BUILTIN, HIGH);
+
+
+    while (central.connected()) {
+      distance = getDistance();
+      distanceSensorCharacteristic.writeValue(distance);
+      delay(200);            
+    }    
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.print("Disconnected from central: ");
+  Serial.println(central.address());
+}
+
+int getDistance(){
+
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
   digitalWrite(triggerPin, HIGH);
@@ -34,4 +78,5 @@ void loop() {
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
+  return distance;  
 }
