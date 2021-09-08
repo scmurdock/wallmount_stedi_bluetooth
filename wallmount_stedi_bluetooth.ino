ab2e1c5355cbe0c420c12f5d7b8c4ced5eb3dfc7
Sean Murdock <scmurdock@gmail.com>
2021-09-08 12:16:31 -0600

    Remove old non-working code

--- wallmount_stedi_bluetooth.ino
+++ wallmount_stedi_bluetooth.ino
@@ -9,7 +9,7 @@
  * 
  */
 #include <ArduinoBLE.h>
-#include <NewPing.h>
+#include <NewPing.h> //this library has built in interrupts, and median filtering
 
 #define TRIGGER_PIN 2
 #define ECHO_PIN 3 
@@ -23,8 +23,6 @@ BLEUnsignedIntCharacteristic distanceSensorCharacteristic("1212", BLERead | BLEN
 int triggerPin=2;
 int echoPin=3;
 
-
-
 volatile unsigned long pulseInTimeBegin = micros();//Returns the number of microseconds since the Arduino board began running the current program
 volatile unsigned long pulseInTimeEnd = micros();//Returns the number of microseconds since the Arduino board began running the current program
 volatile bool newPulseDurationAvailable = false;// volatile ensures the variable is stored in main memory, not in registers, ensuring that it is accessible - this variable is a semaphore used to signal to the main thread that some new data is ready
@@ -32,9 +30,6 @@ volatile bool awaitingEchoFromSensor = false;//this is a semaphore variable used
 
 long duration;
 int distance=0;
-
-
-//below code and more copied from https://forum.arduino.cc/t/non-blocking-ultrasonic-sensor-without-interrupts-and-timers/382076/10
 
 enum us_sensor_phase { 
   trigger, // Phase, in which the US sensor is triggered to send pings.
@@ -45,27 +40,6 @@ enum us_sensor_phase {
 enum us_sensor_phase ultrasonicPhase = trigger; // The current phase of the sensor
 unsigned long ultrasonicPhaseTime = 0;          // Remembers when the phase started.
 
-//
-//void distanceSensorInterrupt()
-//{
-// Serial.println("Interrupt occurred");
-////  Serial.print("Value of awaitingEchoFromSensor: ");
-////  Serial.println(awaitingEchoFromSensor);
-////  Serial.print("Value of newPulseDurationAvailable: ");
-////  Serial.println(newPulseDurationAvailable);
-////  
-////  if (awaitingEchoFromSensor){
-//    if (digitalRead(echoPin) == HIGH){//the echoPin is the one that sends the HIGH/LOW at the beginning and end of the time duration
-//        pulseInTimeBegin = micros();//this is the start of the timer, so we save the time
-//    }
-//    else{
-//      pulseInTimeEnd = micros(); // went from HIGH to LOW, indicating the end of the duration
-//      newPulseDurationAvailable = true;
-////      Serial.println("New duration made available from interrupt");
-////      awaitingEchoFromSensor = false;//we got our answer!
-//    }
-////  }
-//}
 
 void setup(){
   pinMode(triggerPin, OUTPUT);
@@ -73,8 +47,6 @@ void setup(){
   pinMode(LED_BUILTIN, OUTPUT);
   
   Serial.begin(9600);
-
-  //attachInterrupt(digitalPinToInterrupt(echoPin), distanceSensorInterrupt, CHANGE);//attach the function distanceSensorInterrupt to the echoPin so that it gets called whenever the value changes
 
   if(!BLE.begin())
   {
@@ -112,65 +84,6 @@ void loop(){
   Serial.println(distance);
   Serial.println("*****End Variable Values****");
 
-  distance = sonar.ping_cm();
-//
-//  switch(ultrasonicPhase){
-//    case trigger:
-//      ultrasonicPhase = sending;
-//      digitalWrite(triggerPin, LOW);
-//      delayMicroseconds(2);
-//      digitalWrite(triggerPin, HIGH);
-//      delayMicroseconds(10);
-//      digitalWrite(triggerPin, LOW);
-//      ultrasonicPhaseTime = micros();
-//      break;
-//    case sending:
-//      // Wait until all pings have been sent by the ultrasonic sensor.
-////      if((micros() - ultrasonicPhaseTime) > 500UL ) {
-////      // Exit if pings are not sent fast enough
-////        Serial.println("Error1: Ping not sent");
-////        ultrasonicPhase = trigger;
-////      } else 
-//      
-//      if(digitalRead(echoPin) == HIGH) {
-//        ultrasonicPhase = receive;
-//        ultrasonicPhaseTime = micros();
-//      }
-//      break;
-//    case receive:
-//    // Await the echo of a ping.
-//      if((micros() - ultrasonicPhaseTime) > 20000UL) {
-//        // Exit if no echo is received (e.g. because too far away)
-//        Serial.println("Error2: No echo received");
-//        ultrasonicPhase = trigger;
-//      } else if(digitalRead(echoPin) == LOW) {
-//        duration = (micros() - ultrasonicPhaseTime);
-//        distance = duration / 58.2;
-//        ultrasonicPhase = trigger;
-//      }
-//      break;
-//  }
-
-      
-    
-  
-
-//
-//  if (newPulseDurationAvailable){//this variable is a semaphore used to signal to the main thread that some new data is ready
-//    newPulseDurationAvailable = false;//reset the flag so that on the next loop we don't re-read the same data twice
-//    unsigned long pulseDuration = pulseInTimeEnd - pulseInTimeBegin;
-//    distance = pulseDuration * .034 /2; //speed of sound wave divided by 2 (go and back)
-//    Serial.print("Received signal from interrupt value of distance received: ");
-//    Serial.print(distance);
-//    Serial.println(" cm");
-//
-//    Serial.println("Sending trigger to trigger pin, awaiting response...");
-//    digitalWrite(triggerPin, LOW);
-//    delayMicroseconds(2);
-//    digitalWrite(triggerPin, HIGH);
-//    delayMicroseconds(10);
-//    digitalWrite(triggerPin,LOW);    
-//  }
 
   BLEDevice central = BLE.central();
 
@@ -193,17 +106,7 @@ void loop(){
     Serial.println(central.address());
   }
 
-//  if (!awaitingEchoFromSensor){//send request for an echo using trigger pin
-//    
-//    Serial.println("Sending trigger to trigger pin, awaiting response...");
-//    digitalWrite(triggerPin, LOW);
-//    delayMicroseconds(2);
-//    digitalWrite(triggerPin, HIGH);
-//    delayMicroseconds(10);
-//    digitalWrite(triggerPin,LOW);
-//
-//    awaitingEchoFromSensor=true;//we now are waiting for an answer
-//  }
+
   
   delay(50);            
 }
